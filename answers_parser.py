8f72a0f5904821b738f0778e6cea11438efe373c
Vladislav Bondarev <bond7052@gmail.com>
2017-12-25 16:39:52 +0500

    Mister Cool Commit

--- answers_parser.py
+++ answers_parser.py
@@ -1,5 +1,5 @@
-import logging, requests, json
-import utils
+import logging
+import requests, json, utils
 
 logger = logging.getLogger('mail_ru_parser')
 
@@ -7,7 +7,8 @@ logger = logging.getLogger('mail_ru_parser')
 def get_answers(question):
     try:
         clear_question = utils.clear_html_tags(question)
-        r = requests.get('https://go.mail.ru/answer_json?q=' + clear_question)
+        r = requests.get('https://go.mail.ru/answer_json?q=' + clear_question, timeout=20)
+        # logger.info('request time: %s' % (r.elapsed.total_seconds()))
         j = json.loads(r.content.decode(r.encoding))
         answers = []
         for result in j['results']:
@@ -17,5 +18,5 @@ def get_answers(question):
                 answers.append(result['answer'])
         return answers
     except:
-        logger.error('Houston, We\'ve Got a Problem With Mail.ru')
+        logger.warning('Houston, We\'ve Got a Problem With Mail.ru')
         return []
