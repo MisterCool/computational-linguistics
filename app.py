9443f7683dbc21751812b1374f763137022fa881
OlyaBuzilowa <buzybuzy794@gmail.com>
2017-12-26 15:25:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,36 @@
 import logging, os
+import solver, iq_test
 
-  import solver, iq_test
 
- 
-
- 
-
-  def run(qid):
-
+def run(qid):
     try:
-
         qc = iq_test.init_session(qid)
-
         ticket_id = iq_test.get_ticket(qid)
 
- 
-
         if not os.path.exists('logs'):
-
             os.makedirs('logs')
-
-        logging.basicConfig(format=u'[%(levelname)-8s] [%(name)s] [%(asctime)s]  %(message)s',
-
+        logging.basicConfig(format=u'[%(levelname)s] [%(name)s] [%(asctime)s]  %(message)s',
                             filename='logs/%s.log' % ticket_id, level=logging.INFO)
 
- 
-
         logging.info('ticket_id: %s' % ticket_id)
-
         print('ticket_id: %s' % ticket_id)
 
- 
-
         for i in range(qc):
-
             logging.info('question #%i' % (i + 1))
-
+            print(i + 1)
             q_obj = iq_test.get_question(ticket_id)
-
             question = q_obj['questionText']
-
             logging.info('question: %s' % question)
-
             answer = solver.get_possible_answer(question)
-
             logging.info('answer: %s' % answer)
-
             iq_test.save_question(answer, ticket_id, q_obj['questionId'])
 
- 
-
         lnk = iq_test.get_result_link(ticket_id)
-
         logging.info('result: %s' % lnk)
-
         print('result: %s' % lnk)
-
     except Exception as e:
-
         print('Houston, We\'ve Got a Problem.')
-
         raise e
-run(36)
+
+
+run(62)
